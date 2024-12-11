09861c76e16dd418757e7b72b1e6ad9431c45bd9
Hieutm0507 <hieutm.22bi13161@usth.edu.vn>
2024-12-11 09:41:02 +0700

    Update exercises week 4

---
+++ InternshipWeek4/app/src/main/java/com/example/internshipweek4/LoginPage1.kt
@@ -0,0 +1,20 @@
+package com.example.internshipweek4
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginPage1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login_page1)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_page_1)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
