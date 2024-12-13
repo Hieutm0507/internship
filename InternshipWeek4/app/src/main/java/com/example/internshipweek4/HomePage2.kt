cef725326ee3114b7310c8d76438472cb0e7d2ed
Hieutm0507 <hieutm.22bi13161@usth.edu.vn>
2024-12-14 03:56:39 +0700

    Update bài tập 2

--- InternshipWeek4/app/src/main/java/com/example/internshipweek4/HomePage2.kt
+++ InternshipWeek4/app/src/main/java/com/example/internshipweek4/HomePage2.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomePage1 : AppCompatActivity() {
+class HomePage2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home_page1)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ll_home_page_1)) { v, insets ->
+        setContentView(R.layout.activity_home_page2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ll_home_page_2)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
