687390b4edf39b590d3debcb42c7099e5da11b2b
Hieutm0507 <hieutm.22bi13161@usth.edu.vn>
2024-12-15 23:39:33 +0700

    Updated layout Bài 1

---
+++ InternshipWeek5/app/src/main/java/com/example/internshipweek5/ResultActivity.kt
@@ -0,0 +1,24 @@
+package com.example.internshipweek5
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_result_prog1)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun tinhBMI() {
+
+    }
+}
