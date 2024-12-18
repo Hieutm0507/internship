c9d29206b058eb3c48e99ddd666199060007799f
Hieutm0507 <hieutm.22bi13161@usth.edu.vn>
2024-12-18 11:10:26 +0700

    Update Chuyển sang ViewBinding

--- InternshipWeek5/app/src/main/java/com/example/internshipweek5/MainActivity.kt
+++ InternshipWeek5/app/src/main/java/com/example/internshipweek5/MainActivity.kt
@@ -1,23 +1,43 @@
 package com.example.internshipweek5
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.internshipweek5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "BONJOUR"
+    }
+    // Define variables
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.main)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding.btGuiKQ.setOnClickListener {
+            // Lấy giá trị từ EditText
+            val sendHeight = binding.etHeight.text.toString().toInt()
+            //  Method 2
+            //  val inputHeight = height.text.toString()
+            //  val sendHeight = Integer.parseInt(inputHeight)
+            val sendWeight = binding.etWeight.text.toString().toInt()
+            Log.d(TAG,"$sendHeight $sendWeight")
+
+            // Tạo intent
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("Send Height", sendHeight)
+            intent.putExtra("Send Weight", sendWeight)
+            startActivity(intent)
         }
+
+        binding.tvPreResult.text = "Chiều cao: \tCân nặng: \nChỉ số BMI: \tThuộc loại:"
     }
 }
--- InternshipWeek5/app/src/main/java/com/example/internshipweek5/ResultActivity.kt
+++ InternshipWeek5/app/src/main/java/com/example/internshipweek5/ResultActivity.kt
@@ -1,24 +1,64 @@
 package com.example.internshipweek5
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.internshipweek5.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var bindingResult: ActivityResultBinding
+
+    val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {uri: Uri? ->
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_result_prog1)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        bindingResult = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(bindingResult.root)
+
+        // Nhận HEIGHT, WEIGHT và tính BMI
+        val intent = intent
+        val receiveHeight = intent.getIntExtra("Send Height", 0)
+        val receiveWeight = intent.getIntExtra("Send Weight", 0)
+        Log.d("NHẬN SỐ LIỆU", "$receiveHeight, $receiveWeight")
+        val bmi = tinhBMI(receiveHeight, receiveWeight)
+        bindingResult.tvResultBmi.text = "$bmi"
+
+        // Đánh giá chỉ số BMI
+        bindingResult.tvResultType.text = danhGiaBMI(bmi)
+
+        // Quay lại
+        bindingResult.btGoBack.setOnClickListener {
+            finish()
         }
+
     }
 
-    fun tinhBMI() {
+    private fun tinhBMI(height: Int, weight:Int):Double {
+        val getHeight = height.toDouble()
+        val getWeight = weight.toDouble()
 
+        val bmi = getWeight/(getHeight*getHeight/10000.0)
+        return (Math.round(bmi*100.0)/100.0)
+    }
+
+    private fun danhGiaBMI(bmi:Double): String {
+        val result : String = if (bmi < 18.5) {
+            "Thiếu cân"
+        } else if (bmi >= 18.5 && bmi < 23.0) {
+            "Bình thường"
+        } else if (bmi >= 23.0 && bmi < 25.0)
+            "Thừa cân"
+        else if (bmi >= 25.0 && bmi < 30.0)
+            "Béo phì độ 1"
+        else
+            "Béo phì độ 2"
+
+        return result
     }
 }
